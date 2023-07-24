2522734dff2461bbce545b45527e767321be4788
Gabriel Martins <gabrilopesmt@gmail.com>
2023-07-24 20:30:19 -0300

    modificação de codigo

--- app/model/User.php
+++ app/model/User.php
@@ -5,10 +5,9 @@ use app\model\Read;
 
 class User extends Read
 {
-    public $table = ['name' => 'gabriel', 'email' => 'gabrilopesmt@gmail.com', 'password' => '123'];
+    protected static $table = ['name' => 'gabriel', 'email' => 'gabrilopesmt@gmail.com', 'password' => '123'];
     public static function user(){
        $instance = new self();
        return $instance->getTable(); 
     }
-
 }
